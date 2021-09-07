63638b07ee37d392276c59d0921b22c065625872
wudi <acgrid@gmail.com>
2021-09-07 17:42:38 +0800

    Style: Unify esLint and Prettier

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import Vue from 'vue';
-import App from './app.vue';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
+import Vue from 'vue'
+import App from './app.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
 import {
-	faSignOutAlt,
-	faSignInAlt,
-	faUserPlus,
-} from '@fortawesome/free-solid-svg-icons';
+  faSignOutAlt,
+  faSignInAlt,
+  faUserPlus,
+} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faSignInAlt);
-library.add(faSignOutAlt);
-library.add(faUserPlus);
+library.add(faSignInAlt)
+library.add(faSignOutAlt)
+library.add(faUserPlus)
 
-Vue.component('FontAwesomeIcon', FontAwesomeIcon);
+Vue.component('FontAwesomeIcon', FontAwesomeIcon)
 
 export default new Vue({
-	render: (h) => h(App),
-}).$mount('#app');
+  render: (h) => h(App),
+}).$mount('#app')
